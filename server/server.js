b76c800d573797753b060bb474104e6ee1b338ad
ben <ben@mail.com>
2022-09-14 09:26:34 +0200

    Signed-off-by: chart react ben <ben@mail.com> Signed-off-by: ben <ben@mail.com>

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const PORT = 8000;
+
+const server = app.listen(PORT, () => {
+    console.log('server is running on port' + PORT)
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log("socket=",socket.id);
+    socket.on('CLIENT_MSG', data => {
+        console.log("msg=", data);
+        io.emit('SEVER_MSG', data);
+    })
+});
+
